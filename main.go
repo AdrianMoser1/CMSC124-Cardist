a5e5aa70c231fe94ced856dc2cee817ec6a0e250
LuneleIven <lsmoscoso@up.edu.ph>
2026-09-07 20:55:07 +0800

    refactor: changed imports and module name to accomodate scanner

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "cardist/scanner"
+)
 
 func main() {
-    fmt.Println("No, no, it's no use, Jo, jo we gotta have it out. I have loved you ever since I've known you, Jo, I couldn't help it, and and I tried to show you and you wouldn't let me... and I must make you hear now and give me an answer because I cannot go on like this any longer. I gave up billiards, I gave up everything you didn't like, I'm happy I did, it's fine, and I waited and I never complained... You know, I figured you'd love me, Jo, and I realized I'm not half good enough and I'm not this great man.")
+    code := "({\n+ - = \n})"
+    scn := scanner.NewScanner(code)
+    tokens := scn.ScanTokens()
+
+    for _, tok := range tokens {
+        fmt.Println(tok)
+    }
 }
