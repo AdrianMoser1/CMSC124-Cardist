8c5aab90101200d5c44c445902f8b43dab705a0c
LuneleIven <lsmoscoso@up.edu.ph>
2026-09-21 20:58:22 +0800

    refactor: added support for parse files

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"cardist/parser"
 	"cardist/scanner"
 	"fmt"
 	"os"
@@ -13,18 +14,20 @@ func main() { //checks the command line arguments and runs the appropriate mode:
 	switch { //chooses which mode to run based on the command line arguments
 	case len(args) == 0: //runs the REPL
 		runPrompt()
-	case len(args) == 2 && args[0] == "--tokenize": //if the user wants to tokenize a file, it will call runFile with the file path
-		runFile(args[1])
+	case len(args) == 2 && args[0] == "--tokenize": //if the user wants to tokenize a file, it will call runTokenizeFile with the file path
+		runTokenizeFile(args[1])
+	case len(args) == 2 && args[0] == "--parse": //if the user wants to parse a file, it will call runParseFile with the file path
+		runParseFile(args[1])
 	case len(args) == 1: //Prints out an error message and exit with code 70 (unhandled error since its yet to be implemented)
-		fmt.Fprintln(os.Stderr, "Error: execution is not implemented until Lab 4. Use --tokenize <file>.") //
+		fmt.Fprintln(os.Stderr, "Error: execution is not implemented until Lab 4. Use --tokenize <file> or --parse <file>.")
 		os.Exit(70)
 	default: //if the user provides invalid arguments, it will print out the usage message and exit with code 64 (invalid command line usage)
-		fmt.Fprintln(os.Stderr, "Usage: run [--tokenize] [file]")
+		fmt.Fprintln(os.Stderr, "Usage: run [--tokenize | --parse] [file]")
 		os.Exit(64)
 	}
 }
 
-func runFile(path string) {
+func runTokenizeFile(path string) {
 	data, err := os.ReadFile(path)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: could not read file %q: %v\n", path, err)
@@ -34,8 +37,6 @@ func runFile(path string) {
 	scn := scanner.NewScanner(string(data))
 	tokens := scn.ScanTokens()
 
-	// Diagnostics already went to stderr, inside the scanner
-	// stdout only prints the token stream if the scan came back clean
 	if scn.ErrorFound() {
 		os.Exit(65)
 	}
@@ -46,25 +47,65 @@ func runFile(path string) {
 	os.Exit(0)
 }
 
+func runParseFile(path string) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: could not read file %q: %v\n", path, err)
+		os.Exit(66)
+	}
+
+	scn := scanner.NewScanner(string(data))
+	tokens := scn.ScanTokens()
+
+	if scn.ErrorFound() {
+		os.Exit(65)
+	}
+
+	prs := parser.NewParser(tokens)
+	expressions := prs.Parse()
+
+	if prs.ErrorFound() {
+		os.Exit(65)
+	}
+
+	printer := parser.NewAstPrinter()
+	for _, expr := range expressions {
+		if expr != nil {
+			fmt.Println(printer.Print(expr))
+		}
+	}
+	os.Exit(0)
+}
+
 /*
-runPrompt is the REPL: scans one line at a time.  Marking errors but still proceeds
-it just prints its error and the prompt returns
+runPrompt is the REPL: scans and parses one line at a time.
 */
 func runPrompt() {
 	reader := bufio.NewScanner(os.Stdin)
+	printer := parser.NewAstPrinter()
 	fmt.Print("> ")
 	for reader.Scan() {
 		line := reader.Text()
 		scn := scanner.NewScanner(line)
 		tokens := scn.ScanTokens()
-		for _, tok := range tokens {
-			fmt.Println(tok)
+
+		if !scn.ErrorFound() {
+			prs := parser.NewParser(tokens)
+			expressions := prs.Parse()
+
+			if !prs.ErrorFound() {
+				for _, expr := range expressions {
+					if expr != nil {
+						fmt.Println(printer.Print(expr))
+					}
+				}
+			}
 		}
 		fmt.Print("> ")
 	}
 
 	if err := reader.Err(); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: reading input: %v\n", err)
-		os.Exit(1) // indicates an error while reading input from the user
+		os.Exit(1)
 	}
 }
